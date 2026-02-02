f9f7a1de1c1492888acc6d6a29e79dff4d5c9881
4rielo <ascarafia@gmail.com>
2026-02-01 23:12:35 -0300

    Upgraded to v0.2.3
    Unified JVM dependency to 17

--- bambini-core/build.gradle.kts
+++ bambini-core/build.gradle.kts
@@ -13,7 +13,7 @@ kotlin {
 
     androidTarget {
         compilerOptions {
-            jvmTarget.set(JvmTarget.JVM_11)
+            jvmTarget.set(JvmTarget.JVM_17)
         }
     }
 
@@ -38,22 +38,6 @@ kotlin {
             }
         }
     }
-
-    /*
-    //androidTarget()
-    listOf(
-        iosArm64(),
-        iosSimulatorArm64()
-    ).forEach { iosTarget ->
-        iosTarget.binaries.framework {
-            baseName = "ComposeApp"
-            isStatic = true
-        }
-    }
-
-    jvm("desktop")
-    */
-
 }
 
 android {
@@ -75,8 +59,8 @@ android {
         }
     }
     compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_11
-        targetCompatibility = JavaVersion.VERSION_11
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
     }
 }
 
