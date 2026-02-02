14a8a74c8d0f2cfdb290e6153a9795ee958164ac
4rielo <ascarafia@gmail.com>
2026-02-02 11:26:50 -0300

    Upgraded to v0.2.7
    AccountDataSource methods were suspend because of datastore.
    Made regular because now it uses sharedPref

--- bambini-core/src/commonMain/kotlin/com/ascarafia/bambinicore/data/datasource/AccountLocalDataSource.kt
+++ bambini-core/src/commonMain/kotlin/com/ascarafia/bambinicore/data/datasource/AccountLocalDataSource.kt
@@ -7,34 +7,34 @@ import com.ascarafia.bambinicore.domain.datasource.AccountDataSource
 class AccountLocalDataSource(
     private val settingsManager: SettingsManager
 ): AccountDataSource {
-    override suspend fun saveToken(token: String) {
+    override fun saveToken(token: String) {
         settingsManager.putString(LocalStorageKeys.TOKEN.name, token)
     }
 
-    override suspend fun getToken(): String? {
+    override fun getToken(): String? {
         return settingsManager.getString(LocalStorageKeys.TOKEN.name)
     }
 
-    override suspend fun saveRefreshToken(refreshToken: String) {
+    override fun saveRefreshToken(refreshToken: String) {
         settingsManager.putString(LocalStorageKeys.REFRESH_TOKEN.name, refreshToken)
     }
 
-    override suspend fun getRefreshToken(): String? {
+    override fun getRefreshToken(): String? {
         return settingsManager.getString(LocalStorageKeys.REFRESH_TOKEN.name)
     }
-    override suspend fun saveLastUpdate(lastUpdate: String) {
+    override fun saveLastUpdate(lastUpdate: String) {
         settingsManager.putString(LocalStorageKeys.LAST_UPDATE.name, lastUpdate)
     }
 
-    override suspend fun getLastUpdate(): String? {
+    override fun getLastUpdate(): String? {
         return settingsManager.getString(LocalStorageKeys.LAST_UPDATE.name)
     }
 
-    override suspend fun getAccountId(): String? {
+    override fun getAccountId(): String? {
         return settingsManager.getString(LocalStorageKeys.ACCOUNT_ID.name)
     }
 
-    override suspend fun saveAccountId(accountId: String) {
+    override fun saveAccountId(accountId: String) {
         settingsManager.putString(LocalStorageKeys.ACCOUNT_ID.name, accountId)
     }
 }
--- bambini-core/src/commonMain/kotlin/com/ascarafia/bambinicore/domain/datasource/AccountDataSource.kt
+++ bambini-core/src/commonMain/kotlin/com/ascarafia/bambinicore/domain/datasource/AccountDataSource.kt
@@ -2,19 +2,19 @@ package com.ascarafia.bambinicore.domain.datasource
 
 interface AccountDataSource {
 
-    suspend fun saveToken(token: String)
+    fun saveToken(token: String)
 
-    suspend fun getToken(): String?
+    fun getToken(): String?
 
-    suspend fun saveRefreshToken(refreshToken: String)
+    fun saveRefreshToken(refreshToken: String)
 
-    suspend fun getRefreshToken(): String?
+    fun getRefreshToken(): String?
 
-    suspend fun saveLastUpdate(lastUpdate: String)
+    fun saveLastUpdate(lastUpdate: String)
 
-    suspend fun getLastUpdate(): String?
+    fun getLastUpdate(): String?
 
-    suspend fun getAccountId(): String?
+    fun getAccountId(): String?
 
-    suspend fun saveAccountId(accountId: String)
+    fun saveAccountId(accountId: String)
 }
